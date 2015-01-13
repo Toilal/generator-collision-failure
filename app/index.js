a5afc86ebaf09dc3369a4efba24b01d2904fad85
Vyacheslav Rusakov <vyarus@gmail.com>
2015-01-13 12:24:17 +0600

    initial commit

---
+++ app/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+
+  writing: {
+    app: function () {
+      this.fs.copy(
+        this.templatePath('*.jar'),
+        this.destinationRoot()
+      );
+    }
+  }
+});
